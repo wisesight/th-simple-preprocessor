bb5335749346cc9a694a3e071cc4263568c8c493
Taechawat Konkaew <luangta_5@hotmail.com>
2020-10-27 18:12:17 +0700

    Add requirements.txt and edit setup.py

--- setup.py
+++ setup.py
@@ -3,15 +3,19 @@ from setuptools import find_packages, setup
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open("requirements.txt", "r") as fh:
+    requirements = fh.read().splitlines()
+
 setup(
     name="th-simple-preprocessor",
-    version="0.1.2",
+    version="0.2.2",
     author="WISESIGHT Product Development",
     author_email="tequila@wisesight.com",
     description="Simple Thai Preprocess Functions",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/wisesight/th-simple-preprocessor",
+    install_requires=requirements,
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
