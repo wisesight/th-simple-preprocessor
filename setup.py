39521c9f139870450059179532fb98afd18ea9dd
Taechawat Konkaew <luangta_5@hotmail.com>
2022-01-12 20:55:02 +0700

    Bump sub-minor version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="th-simple-preprocessor",
-    version="0.7.0",
+    version="0.7.1",
     author="WISESIGHT Product Development",
     author_email="tequila@wisesight.com",
     description="Simple Thai Preprocess Functions",
