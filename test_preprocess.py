20178ececd5c87b89cf52b896ca8c7a66d507ee1
nirattisai.t <nirattisai.t@wisesight.com>
2022-03-31 15:03:12 +0700

    edit top level domain in RE_LINK and normalize_filename

--- test_preprocess.py
+++ test_preprocess.py
@@ -13,9 +13,9 @@ from th_preprocessor.preprocess import (
     normalize_at_mention,
     normalize_email,
     normalize_emoji,
+    normalize_filename,
     normalize_haha,
     normalize_link,
-    normalize_link_with_extension,
     normalize_num,
     normalize_phone,
     normalize_special_chars,
@@ -43,7 +43,7 @@ class Test_preprocess(object):
         self.unnorm_text = "เเําฤาฦา๑๒๓๔๕๖๗๘๙๐,.=\0\r\n\t\u00A0" + string.punctuation
         self.link_text = "http://www.youtube.com"
         self.link_text_without_protocol = "google.com/search?q=hello"
-        self.link_with_extension = "https://en.wikipedia.org/wiki/Apple_Inc.#/media/File:Apple_logo_black.svg logo.svg"
+        self.filename_text = "logo.png"
         self.mention_text = "@test1234"
         self.email_text = "test_eiei_za@gmail.com"
         self.haha_text = "555555"
@@ -106,12 +106,27 @@ class Test_preprocess(object):
     def test_normalize_link(self):
         expected_result = " WSLINK "
         assert_equal(normalize_link(self.link_text), expected_result)
+
+    def test_normalize_link_without_protocol(self):
         expected_result = " WSLINK "
         assert_equal(normalize_link(self.link_text_without_protocol), expected_result)
-    
-    def test_normalize_link_with_extension(self):
-        expected_result = " WSLINK   WSLINK "
-        assert_equal(normalize_link_with_extension(self.link_with_extension), expected_result)
+
+    def test_normalize_link_text_with_dot(self):
+        text = "This is a book.This is a cat."
+        expected_result = "This is a book.This is a cat."
+        assert_equal(normalize_link(text), expected_result)
+
+    def test_normalize_link_email(self):
+        text = "foo.b@gmail.com"
+        expected_result = "foo.b@gmail.com"
+        assert_equal(normalize_link(text), expected_result)
+        text = "foo_foo@gmail.com"
+        expected_result = "foo_foo@gmail.com"
+        assert_equal(normalize_link(text), expected_result)
+
+    def test_normalize_filename(self):
+        expected_result = " WSFILENAME "
+        assert_equal(normalize_filename(self.filename_text), expected_result)
 
     def test_normalize_at_mention(self):
         expected_result = " WSNAME "
@@ -191,5 +206,6 @@ class Test_preprocess(object):
 
     def test_replace_dup_emojis_with_dup_numbers(self):
         expected_result = "👧 111111 3️⃣"
-        assert_equal(replace_dup_emojis(self.dup_emojis_text_with_dup_numbers), expected_result)
-        
+        assert_equal(
+            replace_dup_emojis(self.dup_emojis_text_with_dup_numbers), expected_result
+        )
