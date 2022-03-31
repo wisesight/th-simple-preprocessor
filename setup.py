64dc25cbfa2f385a7a1f9df55f7f62a1dd336383
nirattisai.t <nirattisai.t@wisesight.com>
2022-03-31 15:39:14 +0700

    update package version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="th-simple-preprocessor",
-    version="0.9.0",
+    version="0.10.0",
     author="WISESIGHT Product Development",
     author_email="tequila@wisesight.com",
     description="Simple Thai Preprocess Functions",
